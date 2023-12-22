e853e7c109dd6d8e603f8aa5d57c9de209d649c0
Fabricio Borgobello <fborgobello@gmail.com>
2023-12-22 13:40:55 +0100

    Improve styling

--- frontend/src/components/TotalBalance.tsx
+++ frontend/src/components/TotalBalance.tsx
@@ -1,32 +1,35 @@
 import { useBalance } from '../hooks/useBalance';
 import { formatCurrency } from '../utils';
+import { clsx } from 'clsx';
 
 export function TotalBalance() {
   const { data: balance, state } = useBalance();
 
-  if (state !== 'success' || !balance) {
-    return <BalanceSkeleton />;
-  }
+  const isLoading = state === 'loading' || !balance;
 
   return (
-    <div className="relative overflow-hidden rounded-md border border-gray-200 bg-white px-5 py-8 sm:pl-8 dark:border-gray-800 dark:bg-gray-800">
+    <div
+      className={clsx(
+        'relative flex flex-col gap-y-1 overflow-hidden rounded-md border border-gray-200 bg-white px-5 py-8 sm:pl-8 dark:border-gray-800 dark:bg-gray-800',
+        isLoading && 'animate-pulse'
+      )}
+    >
       <div className="absolute bottom-0 left-0 h-2 w-full bg-indigo-700 sm:left-0 sm:top-0 sm:h-full sm:w-2"></div>
       <h2 className="text-center text-black opacity-80 sm:text-left  dark:text-white">
         Total balance
-        <span className="ml-1 hidden  text-xs opacity-80 sm:inline">(accross {balance.count} accounts)</span>
+        {!isLoading ? (
+          <span className="ml-1 hidden text-xs opacity-80 sm:inline">(accross 0 accounts)</span>
+        ) : (
+          <span className="ml-1 text-xs opacity-80 sm:inline">(accross {balance?.count} accounts)</span>
+        )}
       </h2>
-      <p className="text-center text-[1.5rem] font-bold text-black sm:text-left  sm:text-4xl dark:text-white">
-        {balance && formatCurrency(balance.balance, balance.currency)}
-      </p>
-    </div>
-  );
-}
-
-function BalanceSkeleton() {
-  return (
-    <div className="flex animate-pulse flex-col  gap-y-1 rounded-xl border border-gray-200 bg-black p-4 text-center sm:text-left dark:bg-white">
-      <div className="h-6 w-1/3 self-center rounded-md bg-gray-300 sm:self-start"></div>
-      <div className="h-10 w-full rounded-md bg-gray-300"></div>
+      {!isLoading ? (
+        <p className="text-center text-[1.5rem] font-bold text-black sm:text-left  sm:text-4xl dark:text-white">
+          {balance && formatCurrency(balance.balance, balance.currency)}
+        </p>
+      ) : (
+        <div className="h-10 w-1/2 rounded-md bg-gray-300"></div>
+      )}
     </div>
   );
 }
