e2e87f6b70b8a9c08428b256cb7cae8cb34ed73f
Fabricio Borgobello <fborgobello@gmail.com>
2023-12-27 10:48:17 +0100

    Reorganize transfer inputs

--- web/src/components/SEPAForm.tsx
+++ web/src/components/SEPAForm.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { FieldError, useForm, UseFormRegister } from 'react-hook-form';
+import { FieldError, useForm, UseFormRegisterReturn } from 'react-hook-form';
 import toast from 'react-hot-toast';
 
 import { useModal } from '@/hooks/useModal';
@@ -59,7 +59,6 @@ export default function SEPAForm() {
       toast.error(error instanceof Error ? error.message : 'Something went wrong. Please try again later.');
     }
   };
-
   return (
     <div className="flex flex-col gap-y-8 p-4 sm:p-8" data-testid="sepa-form">
       <div>
@@ -79,12 +78,17 @@ export default function SEPAForm() {
           </Select>
           {errors.source && <ErrorMessage>{errors.source.message}</ErrorMessage>}
         </div>
-        <FormInput error={errors.amount} id="amount" label="Amount (€)" min={1} register={register} type="number" />
-        <FormInput error={errors.recipient} id="recipient" label="Recipient name" register={register} />
-        <FormInput error={errors.targetIBAN} id="targetIBAN" label="IBAN" register={register} />
-        <FormInput error={errors.targetBIC} id="targetBIC" label="BIC" register={register} />
-        <FormInput error={errors.reference} id="reference" label="Reference" register={register} />
-
+        <FormInput
+          error={errors.amount}
+          id="amount"
+          label="Amount (€)"
+          register={register('amount', { valueAsNumber: true, min: 1 })}
+          type="number"
+        />
+        <FormInput error={errors.recipient} id="recipient" label="Recipient name" register={register('recipient')} />
+        <FormInput error={errors.targetIBAN} id="targetIBAN" label="IBAN" register={register('targetIBAN')} />
+        <FormInput error={errors.targetBIC} id="targetBIC" label="BIC" register={register('targetBIC')} />
+        <FormInput error={errors.reference} id="reference" label="Reference" register={register('reference')} />
         <Button className="last:mt-8" disabled={!formState.isDirty || formState.isSubmitting} type="submit">
           {formState.isSubmitting ? 'Sending...' : 'Send'}
         </Button>
@@ -95,7 +99,7 @@ export default function SEPAForm() {
 
 interface FormInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  register: UseFormRegister<TransferInsert>;
+  register: UseFormRegisterReturn<keyof TransferInsert>;
   error: FieldError | undefined;
 }
 
@@ -103,7 +107,7 @@ function FormInput({ label, register, error, ...rest }: FormInputProps) {
   return (
     <div className="flex flex-col gap-y-1">
       <Label htmlFor={rest.id}>{label}</Label>
-      <Input error={Boolean(error)} {...rest} {...register(rest.id as keyof TransferInsert)} />
+      <Input error={Boolean(error)} {...rest} {...register} />
       {error && <ErrorMessage>{error.message}</ErrorMessage>}
     </div>
   );
